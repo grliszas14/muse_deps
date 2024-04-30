908a32cd12324b234666c5240db1b2357f99464a
Igor Korsukov <igor.korsukov@gmail.com>
2024-04-30 18:10:11 +0300

    added zlib macos rel

--- zlib/1.2.13/zlib.cmake
+++ zlib/1.2.13/zlib.cmake
@@ -1,7 +1,25 @@
 
 function(zlib_Populate remote_url local_path os arch build_type)
 
-    if(os STREQUAL "windows")
+    if(os STREQUAL "macos")
+
+            set(compiler "appleclang15")
+
+            # At the moment only relwithdebinfo
+            # I don't think we need debug builds
+            set(name "zlib_macos_${arch}_relwithdebinfo_${compiler}_os109")
+
+            if (NOT EXISTS ${local_path}/${name}.7z)
+                message(STATUS "[zlib] Populate: ${remote_url} to ${local_path} ${os} ${arch} ${build_type}")
+                file(DOWNLOAD ${remote_url}/${name}.7z ${local_path}/${name}.7z)
+                file(ARCHIVE_EXTRACT INPUT ${local_path}/${name}.7z DESTINATION ${local_path})
+            endif()
+
+            set(zlib_INCLUDE_DIRS ${local_path}/include)
+            set(zlib_LIBRARIES ${local_path}/lib/libz.1.2.13.dylib)
+            set(zlib_INSTALL_LIBRARIES ${zlib_LIBRARIES})
+
+    elseif(os STREQUAL "windows")
 
         set(compiler "msvc192")
         set(suffix "")
